8e1fc84bef9fca506313c3043bcec8c03be1f015
Usman4906 <usmanyameen20@gmail.com>
2025-09-21 15:48:28 +0500

    refactor: update font import and adjust layout structure chore: upgrade @clerk/nextjs dependency to version 6.32.0

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Source_Sans_3 as FontSans } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/common/header";
 import Footer from "@/components/common/footer";
@@ -7,10 +7,9 @@ import { ClerkProvider } from "@clerk/nextjs";
 import { Toaster } from "sonner";
 import { ORIGIN_URL } from "@/utils/helpers";
 
-const fontSans = FontSans({
-  variable: "--font-sans",
+const inter = Inter({
   subsets: ["latin"],
-  weight: ["200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-sans",
 });
 
 export const metadata: Metadata = {
@@ -33,14 +32,12 @@ export default function RootLayout({
 }>) {
   return (
     <ClerkProvider>
-      <html lang="en">
-        <body className={`font-sans ${fontSans.variable} antialiased`}>
+      <html lang="en" suppressHydrationWarning>
+        <body className={`font-sans ${inter.variable} antialiased`} suppressHydrationWarning>
           <div className="flex relative min-h-screen flex-col">
             <Header />
             <main className="flex-1">{children}</main>
-            {/* <Footer /> */}
           </div>
-          <Toaster />
         </body>
       </html>
     </ClerkProvider>
