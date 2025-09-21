c7551924c267bb4f0cf6acd22e5e66d06946e9a7
Usman4906 <usmanyameen20@gmail.com>
2025-09-21 16:56:25 +0500

    fix: correct indentation in ESLint configuration

--- eslint.config.js
+++ eslint.config.js
@@ -2,13 +2,13 @@ import js from '@eslint/js';
 import nextPlugin from '@next/eslint-plugin-next';
 
 export default [
-  js.configs.recommended,
-  {
-    plugins: {
-      next: nextPlugin,
+    js.configs.recommended,
+    {
+        plugins: {
+            next: nextPlugin,
+        },
+        rules: {
+            'next/core-web-vitals': 'error',
+        },
     },
-    rules: {
-      'next/core-web-vitals': 'error',
-    },
-  },
 ];
